b2add46e0bbf8fa90d343d6dc09e29cebc99f96d
Kramzis1 <kramekm95@gmail.com>
2024-10-15 12:23:09 +0200

    Initial Commit

---
+++ lab1/lab1App.swift
@@ -0,0 +1,17 @@
+//
+//  lab1App.swift
+//  lab1
+//
+//  Created by Magda on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct lab1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
